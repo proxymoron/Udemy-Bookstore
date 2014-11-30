fb46ed94b6a8f05f33f7d9226ae93cbbc6ed1ac2
Robert Guss <robertguss@gmail.com>
2014-11-30 17:29:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Udemy: Bookstore Sample App
+
+This is the BookStore sample application for
+[*Udemy: Learn Ruby on Rails by building Projects](www.udemy.com/learn-ruby-on-rails-building-projects)
+
+by [Eduonix Learning Solutions](http:www.eduonix.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookstore::Application.config.secret_key_base = '99462117eb89195e2d2deb7639879f0922580f27e28ec7e9071c1b4c12d551d097a916cb614650e4caddedf96c4cf71c1484c090bc17c14110a171110a35dba2'
